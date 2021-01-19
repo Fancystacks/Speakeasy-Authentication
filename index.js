53e7711c1458c9b781dd15222b693033f9c8a387
Fancy Stacks <fancystacks@protonmail.com>
2021-01-18 19:02:54 -0700

    register a user 🔐

--- index.js
+++ index.js
@@ -6,10 +6,26 @@ const { Config } = require('node-json-db/dist/lib/JsonDBConfig');
 
 const app = express();
 
-const db = new JsonDB(new Config("myDataBase", true, false, '/'));
-
 const PORT = process.env.PORT || 5000;
 
+const db = new JsonDB(new Config("myDataBase", true, false, '/'));
+
+// register user & create temporary secret 
+app.post('/api/register', (req, res) => {
+    const id = uuid.v4();
+
+    try {
+        const path = `/user/${id}`
+        const temp_secret = speakeasy.generateSecret();
+        db.push(path, { id, temp_secret });
+        res.json({ id, secret: temp_secret })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Error generating the secret '});
+    }
+});
+
 app.get('/api', (req, res) => res.json({ message: ' Welcome to the two-factor authentication example'}));
+
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
