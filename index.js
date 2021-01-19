0162c6724ab9d980410c579a6d456bde3f92d846
Fancy Stacks <fancystacks@protonmail.com>
2021-01-18 19:21:47 -0700

    verify user 🔐

--- index.js
+++ index.js
@@ -17,19 +17,41 @@ app.post('/api/register', (req, res) => {
     const id = uuid.v4();
 
     try {
-        const path = `/user/${id}`
+        const path = `/user/${id}`;
         const temp_secret = speakeasy.generateSecret();
         db.push(path, { id, temp_secret });
         res.json({ id, secret: temp_secret.base32 })
     } catch (error) {
         console.log(error);
-        res.status(500).json({ message: 'Error generating the secret '});
+        res.status(500).json({ message: 'Error generating the secret'});
     }
 });
 
 // verify secret, make secret premanent 
 app.post('/api/verify', (req, res) => {
-    req.body
+    const {token, userId} = req.body
+
+    try {
+        const path = `/user/${id}`;
+        const user = db.getData(path);
+
+        const { base32: secret } = user.temp_secret
+
+        const verified = speakeasy.totp.verify({ secret,
+            encoding: 'base32',
+            token });
+
+        if(verified) {
+            db.push(path, { id: userId, secret: user.temp_secret })
+            res.json({ verified: true });
+        } else {
+            res.json({ verified: false });
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Error finding the user'});
+    }
 });
 
 app.get('/api', (req, res) => res.json({ message: ' Welcome to the two-factor authentication example'}));
