be9f39c57182e63cee03dae41636d611dc8f5f24
Fancy Stacks <fancystacks@protonmail.com>
2021-01-18 19:11:46 -0700

    express middleware 🔐

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const { Config } = require('node-json-db/dist/lib/JsonDBConfig');
 
 const app = express();
 
+app.use(express.json());
+
 const PORT = process.env.PORT || 5000;
 
 const db = new JsonDB(new Config("myDataBase", true, false, '/'));
@@ -18,11 +20,16 @@ app.post('/api/register', (req, res) => {
         const path = `/user/${id}`
         const temp_secret = speakeasy.generateSecret();
         db.push(path, { id, temp_secret });
-        res.json({ id, secret: temp_secret })
+        res.json({ id, secret: temp_secret.base32 })
     } catch (error) {
         console.log(error);
         res.status(500).json({ message: 'Error generating the secret '});
     }
+});
+
+// verify secret, make secret premanent 
+app.post('/api/verify', (req, res) => {
+    req.body
 });
 
 app.get('/api', (req, res) => res.json({ message: ' Welcome to the two-factor authentication example'}));
